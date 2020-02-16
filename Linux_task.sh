368007bc05d002de7574ec11d9dd6eae47408b2c
Shari87 <49628483+Shari87@users.noreply.github.com>
2020-02-16 23:03:19 +0530

    Delta changes

--- Linux_task.sh
+++ Linux_task.sh
@@ -1,1 +1,1 @@
-history | grep -wc "sudo" | grep -i "sudo" ~/.bash_history | head -n10 | sort -nr | nl | grep "sudo" | awk '{CMD[$3]++count++;} END{ for (a in CMD) print CMD[a] " " a;}'|sort -nr
+history | grep -wc "sudo" | grep -i "sudo" ~/.bash_history | head -n10 | sort -nr | nl | grep "sudo" | awk '{CMD[$3]++count++;} END{ for (a in CMD) print CMD[a] " " a;}'| sort -nr | nl
